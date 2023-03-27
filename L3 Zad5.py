572f1d3c9f8bd5bf02e9282b93ad4d8966e53396
JakubSzczech3 <116097926+JakubSzczech3@users.noreply.github.com>
2023-03-27 19:35:59 +0200

    Update L3 Zad5.py

--- L3 Zad5.py
+++ L3 Zad5.py
@@ -1,9 +1,9 @@
-def han(a, x, y, g):
-    if a == 1:
-        print("Przesuń dysk 1 z kijka ", x, "na kijek ", y)
+def han(a,x,y,g):
+    if a==1:
+        print("Przesuń dysk 1 z kijka ",x,"na kijek ",y)
         return
-    han(a - 1, x, g, y)
-    print("Przesuń dysk ", a, "z kijka ", x, "na kijek ", y)
-    han(a - 1, g, y, x)
+    han(a-1,x,g,y)
+    print("Przesuń dysk ",a,"z kijka ",x,"na kijek ",y)
+    han(a-1,g,y,x)
 a=int(input("podaj liczbe krążków: "))
-han(a, '1', '2', '3')
+han(a,'1','2','3')
