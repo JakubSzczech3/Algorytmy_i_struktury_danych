2eca40e8b2bb6dd94ab3f296173016c18469d435
JakubSzczech3 <116097926+JakubSzczech3@users.noreply.github.com>
2023-04-25 09:18:33 +0200

    Add files via upload

---
+++ L7 zad5.py
@@ -0,0 +1,12 @@
+class Queue:
+    def __init__(self):
+        self.items=[]
+    def isEmpty(self):
+        return self.items==[]
+    def enqueue(self,item):
+        sefl.items.insert(0,item)
+    def denqueue(sefl):
+        return self.items.pop()
+    def size(self):
+        return len(self.items)
+
